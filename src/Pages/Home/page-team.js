963a70cd3015050edc7483e0d8b263d5acec928e
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-04-16 00:08:07 +0700

    modify page-team

--- src/Pages/Home/page-team.js
+++ src/Pages/Home/page-team.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import { Col, Card, Row } from 'react-materialize'
 
 class OurTeam extends Component {
     render() {
         return (
-            <div className='wrapper_' style={{
+            <div style={{
                 width: "100%",
-                height: "100vh",
+                height: "100%",
                 position: 'static',
                 backgroundColor: '#FFFFFF'
             }}>
-                <span>
-                    Our Team
-                    </span>
+                <div className='parent' style={{backgroundColor: '#ffffff'
+                }}>
+                    <Row className='child'>
+                        <Col m={3} s={12}>
+                            
+                        </Col>
+                        <Col m={3} s={12}>
+                            <Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
+                                I am a very simple card.
+                            </Card>
+                        </Col>
+                        <Col m={3} s={12}>
+                            <Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
+                                I am a very simple card.
+                            </Card>
+                        </Col>
+                        <Col m={3} s={12}>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='parent' style={{backgroundColor: '#ffffff'
+                }}>
+                    <Row className='child' style={{textAlign:'center'}}>
+                    <h2>CREATED BY EXPERTS</h2>
+We believe in a diverse range of talent and personality brings creative skills and ideas to the web.
+                    </Row>
+                </div>
+
             </div>
         );
     }
